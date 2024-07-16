89c54d80e7add6b98f8ea56fc31a34a4c473ec2a
HIRUMI SENEVIRATHNA <120644649+hirumisenevirathna@users.noreply.github.com>
2024-07-16 21:34:19 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3001;
+const host = 'localhost';
+const mongoose = require('mongoose');
+const router = require('./router');
+
+app.use(cors());
+app.use(express.json());
+
+const uri = 'mongodb+srv://Hirumi:Hirumi3@cluster0.ibmda6w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const connect = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log('Connected to mongodb');
+    } catch (error) { // Declare 'error' here
+        console.log('mongodb error', error); // This will now log the error correctly
+    }
+};
+
+connect();
+
+const server = app.listen(port, host, () => {
+    console.log(`Node server is listening to ${server.address().port}`);
+});
+
+app.use('/api', router);
